42c8151bb778aa24b3cab72c96083cac0af2ba0e
Billy Hughes <w.g.h.hughes@gmail.com>
2021-12-19 19:57:38 +0000

    Add CheckoutService class with rudimentary AddToBasket and GetTotal methods.

---
+++ CheckoutService/CheckoutService.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace CheckoutService
+{
+    public class CheckoutService
+    {
+        private readonly Dictionary<char, int> _catalogue = new();
+        private readonly List<char> _basket = new();
+
+        public CheckoutService(Dictionary<char, int> catalogue)
+        {
+            _catalogue = catalogue;
+        }
+
+        public void AddToBasket(string items)
+        {
+            _basket.AddRange(items);
+        }
+
+        public int GetTotal()
+        {
+            int total = 0;
+
+            foreach (char sku in _basket)
+            {
+                total += _catalogue[sku];
+            }
+
+            return total;
+        }
+    }
+}
